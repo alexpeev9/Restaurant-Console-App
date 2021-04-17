9774bab7108f507182c9c39fb729fe37b1eb2668
alexpeev9 <62713012+alexpeev9@users.noreply.github.com>
2021-04-17 14:20:24 +0300

    Added ExitCommand

---
+++ RestaurantConsoleApp/Commands/ExitCommand.cs
@@ -0,0 +1,24 @@
+﻿namespace RestaurantConsoleApp.Commands
+{
+    using RestaurantConsoleApp.Commands.Interfaces;
+    using System;
+    using System.Threading;
+
+    public class ExitCommand : ICommand
+    {
+        public string Execute(string[] inputArgs)
+        {
+            for (int i = 5; i >= 1; i--)
+            {
+                Console.WriteLine($"Exit after {i}s");
+                Thread.Sleep(1000);
+                Console.Clear();
+            }
+
+            Console.WriteLine("The app was successfully stopped");
+
+            Environment.Exit(0);
+            return null;
+        }
+    }
+}

e94cfa5bbc56e9b5eecc7dc630b3907bd7b908ab
alexpeev9 <62713012+alexpeev9@users.noreply.github.com>
2021-04-17 14:27:05 +0300

    Added AddClientCommand

---
+++ RestaurantConsoleApp/Commands/AddClientCommand.cs
@@ -0,0 +1,38 @@
+﻿using RestaurantConsoleApp.Commands.Interfaces;
+using RestaurantConsoleApp.Factories.Interfaces;
+using RestaurantConsoleApp.Models.Interfaces;
+using RestaurantConsoleApp.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantConsoleApp.Commands
+{
+    public class AddClientCommand : ICommand
+    {
+        private const string SuccessfullyAddedMessage = "Successfully created Client: {0}!";
+
+        private readonly IRepository<IBaseModel> _clientRepository;
+        private readonly IClientFactory _clientFactory;
+
+        public AddClientCommand(IRepository<IBaseModel> clientRepository, IClientFactory clientFactory)
+        {
+            _clientRepository = clientRepository;
+            _clientFactory = clientFactory;
+        }
+
+        public string Execute(string[] inputArgs)
+        {
+            string clientType = inputArgs[0];
+            string username = inputArgs[1];
+            decimal balance = decimal.Parse(inputArgs[2]);
+            var client = _clientFactory.Create(clientType, username, balance);
+
+            _clientRepository.Add(client);
+
+            string result = string.Format(SuccessfullyAddedMessage, username);
+
+            return result;
+        }
+    }
+}
